507410b5b48723e9d6a67e02b1e422103d42bf3d
AdithyaHarith <AdithyaHarith@gmail.com>
2022-09-26 08:13:01 +0530

    add Summation program

---
+++ Summation.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Summation {
+    public static void main(String[] args) {
+        int a;
+        int sum=0;
+
+
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+
+        for(int i=1; i<=a; i++ ){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+
+    }
+}
