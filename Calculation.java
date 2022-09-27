a9c8314a3ea5b2ab8f6c496fe97af78422574bb4
AdithyaHarith <AdithyaHarith@gmail.com>
2022-09-27 19:55:37 +0530

    add Calculation program

--- Calculation.java
+++ Calculation.java
@@ -10,38 +10,74 @@ public class Calculation {
         int number1 = new Scanner(System.in).nextInt();
 
 
-        //int result=add(number, number1);
-        System.out.println("what operation:");
-        String operation = new Scanner(System.in).nextLine();
+        int add = addition(number, number1);
+        int sub = subbraction(number, number1);
+        int mul = multiplication(number, number1);
+        int div = division(number, number1);
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
 
 
-        // If i give + symbol then it should add the given input number and it will display the result
-
-        if (operation.equals("+")) {
-            int sum = number + number1;
-            System.out.println("result:" + sum);
-
-        }
-
-        // if i give - symbol then it should sub the given input number and it will display the result
-        if (operation.equals("-")) {
-            int sum = number - number1;
-            System.out.println("result:" + sum);
-        }
-
-        // if i give * symbol then it should multiply the given input number and it will display the result
-        if (operation.equals("*")) {
-            int sum = number * number1;
-            System.out.println("result:" + sum);
-        }
-
-
-        // if i give / symbol then it should divide the given input number and it will display the result
-
-        if (operation.equals("/")) {
-            int sum = number / number1;
-            System.out.println("result:" + sum);
-        }
     }
 
+
+    public static int addition(int a, int b) {
+        int c = a + b;
+        return c;
+
+    }
+
+    public static int subbraction(int a, int b) {
+        int c = a - b;
+        return c;
+    }
+
+    public static int multiplication(int a, int b) {
+        int c = a * b;
+        return c;
+    }
+
+    public static int division(int a, int b) {
+        int c = a / b;
+        return c;
+    }
 }
+
+
+/*//int result=add(number, number1);
+        System.out.println("what operation:");
+    String operation = new Scanner(System.in).nextLine();
+
+
+    // If i give + symbol then it should add the given input number and it will display the result
+
+        if (operation.equals("+")) {
+        int sum = number + number1;
+        System.out.println("result:" + sum);
+
+    }
+
+    // if i give - symbol then it should sub the given input number and it will display the result
+        if (operation.equals("-")) {
+        int sum = number - number1;
+        System.out.println("result:" + sum);
+    }
+
+    // if i give * symbol then it should multiply the given input number and it will display the result
+        if (operation.equals("*")) {
+        int sum = number * number1;
+        System.out.println("result:" + sum);
+    }
+
+
+    // if i give / symbol then it should divide the given input number and it will display the result
+
+        if (operation.equals("/")) {
+        int sum = number / number1;
+        System.out.println("result:" + sum);
+    }
+}
+*/
